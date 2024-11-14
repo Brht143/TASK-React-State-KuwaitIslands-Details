7561166e452cc30385a2ee72816bebcde3e870f3
Brht143 <i.h.elchami@gmail.com>
2024-11-14 15:56:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import './App.css';
 import Header from './Components/Header';
 import IslandForm from './Components/IslandForm';
@@ -9,17 +9,24 @@ function App() {
 
   let island = {
     name: "Boubyan",
-    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg"
+    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg",
+    visitors: 0
   }
 
+  const [selectedIsland, setSelectedIsland] = useState(island);
+  
+  const [visitors, setVisitors] = useState(selectedIsland.visitors);
+
+  console.log(`New visitor for ${selectedIsland.name}, total: ${visitors}`)
+  
   return (
 
     <div>
       <Header />
 
       <div className="homePage">
-        <IslandList />
-        <IslandForm island={island} />
+        <IslandList handleSelected ={setSelectedIsland}/>
+        <IslandForm island={selectedIsland} setVisitors={setVisitors} />
       </div>
     </div>
   );
